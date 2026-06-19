d1acb6e7aeefea541409dd5e8ed85f8c8c3b4da4
deybu <davebrianlibrea@gmail.com>
2026-06-19 21:01:07 +0800

    point frontend to live API

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://budget-tracker-api-7o3a.onrender.com";
 
 // Redirect to login if not authenticated
 const token = localStorage.getItem("token");
